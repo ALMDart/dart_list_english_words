92f89c5ae5f6ebebb955851285e71fb3e4a48a69
ALMaclaine <almaclaine@gmail.com>
2020-04-25 15:06:16 -0700

    Update example

--- example/list_english_words_example.dart
+++ example/list_english_words_example.dart
@@ -1,6 +1,5 @@
 import 'package:list_english_words/list_english_words.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  print(list_english_words.sublist(0, 50).join('\n'));
 }
