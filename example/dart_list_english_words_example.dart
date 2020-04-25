3f7f4ac5c29c014fc786f93a6a42ecd42fad2298
ALMaclaine <almaclaine@gmail.com>
2020-04-25 12:50:17 -0700

    Push up initial project.

---
+++ example/dart_list_english_words_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_list_english_words/dart_list_english_words.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dart_list_english_words.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_list_english_words;
+
+export 'src/dart_list_english_words_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
